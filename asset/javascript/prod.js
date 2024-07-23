26201412ef04f087c69e880889e2325968fafb2b
AnicetChiza <chizaanicet@gmail.com>
2024-07-23 12:00:53 +0200

    add to my repository

--- asset/javascript/prod.js
+++ asset/javascript/prod.js
@@ -8,3 +8,37 @@ document.querySelectorAll('.prod-type-container').forEach(item => {
         };
     });
 });
+
+document.querySelectorAll('.dropdown-content').forEach(content => {
+    content.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            updateButtonText(content);
+        });
+    });
+});
+
+/*----------------------------------
+#Select color
+----------------------------------*/
+
+function updateButtonText(content) {
+    const checkboxes = content.querySelectorAll('input[type="checkbox"]:checked');
+    const selectedValues = Array.from(checkboxes).map(cb => cb.nextSibling.textContent.trim());
+
+    const button = content.previousElementSibling;
+    if (selectedValues.length > 0) {
+        button.textContent = selectedValues.join(', ');
+    } else {
+        button.textContent = button.id === 'size-btn' ? 'Select Sizes' : 'Select Colors';
+    }
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        document.querySelectorAll('.dropdown-content').forEach(dropdown => {
+            if (dropdown.classList.contains('show')) {
+                dropdown.classList.remove('show');
+            }
+        });
+    }
+};
