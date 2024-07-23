b19006b91179590823fd1b2e9a4046d34eaacfe6
AnicetChiza <chizaanicet@gmail.com>
2024-07-23 12:05:13 +0200

    add to my repository

--- asset/javascript/prod.js
+++ asset/javascript/prod.js
@@ -1,3 +1,6 @@
+/*----------------------------------
+#Changing image position
+----------------------------------*/
 document.querySelectorAll('.prod-type-container').forEach(item => {
     const bigImage = item.querySelector('.big-image'); // Change to querySelector
     const smallImages = item.querySelectorAll('.small-image'); // Scope to the container
