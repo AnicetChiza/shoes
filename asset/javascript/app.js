01e7e55fa668a8ccbc52f48122955140fb61355b
AnicetChiza <chizaanicet@gmail.com>
2024-07-24 16:55:04 +0200

    add to my repository

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -70,3 +70,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+/**************************************
+#Login see password
+***************************************/
+let passwords = document.querySelector('.password');
+let openIcon = document.querySelector('.open');
+let closeIcon = document.querySelector('.close');
+
+passwords.setAttribute('type', 'password');
+
+openIcon.onclick = function () {
+    passwords.setAttribute('type', 'text');
+    openIcon.classList.add('hidden');
+    closeIcon.classList.remove('hidden');
+}
+
+closeIcon.onclick = function () {
+    passwords.setAttribute('type', 'password');
+    openIcon.classList.remove('hidden');
+    closeIcon.classList.add('hidden');
+}
